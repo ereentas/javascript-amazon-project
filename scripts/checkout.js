65db6e0a216c68c157ac55666bc56fcb19f70c0e
ereentas <eren.tas0661@gmail.com>
2025-08-25 17:19:19 +0300

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+
 
 let cartSummaryHTML = '';
 
@@ -39,14 +40,14 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
         </div>
 
         <div class="delivery-options">
-          <div class="delivery-options-title">
+          <div class="delivery-options-title">ß
             Choose a delivery option:
           </div>
           <div class="delivery-option">
@@ -76,7 +77,7 @@ cart.forEach((cartItem) => {
             </div>
           </div>
           <div class="delivery-option">
-            <input type="radio"
+            <input type="radio"ß
               class="delivery-option-input"
               name="delivery-option-${matchingProduct.id}">
             <div>
@@ -96,3 +97,13 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      console.log(cart);
+    });
+  });
