dab12c83cdd7f30a5a3710e1a319d80678da507c
ereentas <eren.tas0661@gmail.com>
2025-08-26 16:24:16 +0300

    ddeneme

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -136,3 +136,12 @@ document.querySelectorAll('.js-delete-link')
     });
   });
 
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+
+    element.addEventListener('click', () => {
+      const productId = element.dataset.productId;
+      const deliveryOptionId = element.dataset.deliveryOptionId;
+      updateDeliveryOption(productId,deliveryOptionId);
+    });
+  });
